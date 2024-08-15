b2316b177644a2a4c721f41fd2e9882bb712c860
ShallMate <gw_ling@sjtu.edu.cn>
2024-08-15 22:37:06 +0800

    update

--- main.cc
+++ main.cc
@@ -39,14 +39,12 @@ int main() {
 
   uint128_t seed;
   prng.Fill(absl::MakeSpan(&seed, 1));
+  size_t elementstart = 10;
+  size_t intersize = n-elementstart;
   std::vector<uint128_t> items_a = CreateRangeItems(0, n);
-  std::vector<uint128_t> items_b = CreateRangeItems(10, n);
+  std::vector<uint128_t> items_b = CreateRangeItems(elementstart, n);
   
   auto lctxs = yacl::link::test::SetupWorld(2);  // setup network
-
-
-  auto start_time = std::chrono::high_resolution_clock::now();
-
   std::future<void> fastpsi_sender = std::async(
       std::launch::async, [&] { FastPsiSend(lctxs[0], items_a, ourokvs); });
 
@@ -56,14 +54,11 @@ int main() {
 
   fastpsi_sender.get();
   auto psi_result = fastpsi_receiver.get();
-  auto end_time = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> duration = end_time - start_time;
-  std::cout << "Execution time: " << duration.count() << " seconds"
-            << std::endl;
-  ;
-
+  if(psi_result.size() == intersize){
+    std::cout<<"The PSI protocol has been successfully executed"<<std::endl;
+  }
   std::sort(psi_result.begin(), psi_result.end());
-  std::cout<<psi_result.size()<<std::endl;
+  std::cout<<"The intersection size is "<<psi_result.size()<<std::endl;
   auto bytesToMB = [](size_t bytes) -> double {
     return static_cast<double>(bytes) / (1024 * 1024);
   };
@@ -78,5 +73,8 @@ int main() {
             << std::endl;
   std::cout << "Receiver received bytes: "
             << bytesToMB(receiver_stats->recv_bytes.load()) << " MB"
+            << std::endl;
+  std::cout << "Total Communication: "
+            << bytesToMB(receiver_stats->sent_bytes.load())+bytesToMB(receiver_stats->recv_bytes.load()) << " MB"
             << std::endl;
 }
